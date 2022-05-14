69216eb82c776c75d2b1f6625d6c339c5f3ea732
Ibrahim <i-m-6@hotmail.com>
2022-05-14 19:47:01 +0300

    Initial Commit

---
+++ FirstUI/FirstUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstUIApp.swift
+//  FirstUI
+//
+//  Created by ibrahim asiri on 13/10/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct FirstUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
