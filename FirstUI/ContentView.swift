22427e37cbcd2dd822d54bf2c4ed553aa9f1897b
Ibrahim <i-m-6@hotmail.com>
2022-09-17 10:33:23 +0300

    add another tab item to tabbar

--- FirstUI/ContentView.swift
+++ FirstUI/ContentView.swift
@@ -58,6 +58,14 @@ struct ContentView: View {
                         Image(systemName: "house.fill")
                         Text("Home")
                     }
+            
+            Text("Bookmark Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "bookmark.circle.fill")
+                        Text("Bookmark")
+                    }
+            
         }
     }
     
