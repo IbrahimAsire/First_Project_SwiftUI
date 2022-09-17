b450c5128d9a50646ee86bdb97d6e1ccf2d7b749
Ibrahim <i-m-6@hotmail.com>
2022-09-17 10:29:42 +0300

    feat: try add tabItems

--- FirstUI/ContentView.swift
+++ FirstUI/ContentView.swift
@@ -6,51 +6,62 @@ struct ContentView: View {
     @State var EmailTF = ""
     @State var PasswordTF = ""
     var body: some View {
-        ZStack {
-            // BackGround
-            Color("BG2")
-            VStack {
-            VStack {
-                CustomShape()
-                    .fill(Color("BG1"))
-                    .frame(width: screenSize.bounds.width, height: screenSize.bounds.width)
-                    .overlay(
-                        Text("UI App")
-                            .font(.system(size: 32, weight: .heavy, design: .rounded))
+        TabView {
+            
+        
+            ZStack {
+                // BackGround
+                Color("BG2")
+                VStack {
+                    VStack {
+                        CustomShape()
+                            .fill(Color("BG1"))
+                            .frame(width: screenSize.bounds.width, height: screenSize.bounds.width)
+                            .overlay(
+                                Text("UI App")
+                                    .font(.system(size: 32, weight: .heavy, design: .rounded))
+                                    .foregroundColor(Color("TextC1"))
+                                    .offset( y: -30)
+                            )
+                        //                Spacer()
+                    }.ignoresSafeArea()
+                    VStack {
+                        Text("Login")
+                            .font(.system(size: 32, weight: .heavy, design: .default))
+                            .padding(.top ,90)
                             .foregroundColor(Color("TextC1"))
-                            .offset( y: -30)
-                    )
-//                Spacer()
-            }.ignoresSafeArea()
-            VStack {
-                Text("Login")
-                    .font(.system(size: 32, weight: .heavy, design: .default))
-                    .padding(.top ,90)
-                    .foregroundColor(Color("TextC1"))
-                
-                CustomTF(placeholder: "Email", isPassword: false, text: $EmailTF)
-                    .padding(.top,50)
-                    .padding()
-                CustomTF(placeholder: "Password", isPassword: true, text: $PasswordTF)
-                    
-                    .padding()
-                Button {
-                    
-                } label: {
-                    Text("Log in")
-                        .font(.system(size: 16,weight: .medium))
-                        .foregroundColor(.white)
-                        .padding(16)
-                        .frame(maxWidth:.infinity)
-                        .background(RoundedRectangle(cornerRadius: 4).fill(Color("TextC2")))
+                        
+                        CustomTF(placeholder: "Email", isPassword: false, text: $EmailTF)
+                            .padding(.top,50)
+                            .padding()
+                        CustomTF(placeholder: "Password", isPassword: true, text: $PasswordTF)
+                        
+                            .padding()
+                        Button {
+                            
+                        } label: {
+                            Text("Log in")
+                                .font(.system(size: 16,weight: .medium))
+                                .foregroundColor(.white)
+                                .padding(16)
+                                .frame(maxWidth:.infinity)
+                                .background(RoundedRectangle(cornerRadius: 4).fill(Color("TextC2")))
+                        }
+                        .padding()
+                        Spacer()
+                    }.offset(y: -screenSize.bounds.width / 1.7)
+                    Spacer()
                 }
-                .padding()
-                Spacer()
-            }.offset(y: -screenSize.bounds.width / 1.7)
-                Spacer()
+                
             }
+            .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
         }
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -62,19 +73,19 @@ struct ContentView_Previews: PreviewProvider {
 
 struct CustomShape: Shape {
     func path(in rect: CGRect) -> Path {
-            var path = Path()
-            let width = rect.size.width
-            let height = rect.size.height
-            path.move(to: CGPoint(x: width, y: -0.33867*height))
-            path.addLine(to: CGPoint(x: 0, y: -0.33867*height))
-            path.addLine(to: CGPoint(x: 0, y: 0.97799*height))
-            path.addCurve(to: CGPoint(x: 0.02415*width, y: 0.93959*height), control1: CGPoint(x: 0.00403*width, y: 0.96338*height), control2: CGPoint(x: 0.01234*width, y: 0.95001*height))
-            path.addLine(to: CGPoint(x: 0.44221*width, y: 0.57057*height))
-            path.addCurve(to: CGPoint(x: 0.55502*width, y: 0.57057*height), control1: CGPoint(x: 0.47409*width, y: 0.54242*height), control2: CGPoint(x: 0.52313*width, y: 0.54242*height))
-            path.addLine(to: CGPoint(x: 0.97307*width, y: 0.93959*height))
-            path.addCurve(to: CGPoint(x: width, y: 0.99848*height), control1: CGPoint(x: 0.99023*width, y: 0.95474*height), control2: CGPoint(x: width, y: 0.9761*height))
-            path.addLine(to: CGPoint(x: width, y: -0.33867*height))
-            path.closeSubpath()
-            return path
-        }
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.move(to: CGPoint(x: width, y: -0.33867*height))
+        path.addLine(to: CGPoint(x: 0, y: -0.33867*height))
+        path.addLine(to: CGPoint(x: 0, y: 0.97799*height))
+        path.addCurve(to: CGPoint(x: 0.02415*width, y: 0.93959*height), control1: CGPoint(x: 0.00403*width, y: 0.96338*height), control2: CGPoint(x: 0.01234*width, y: 0.95001*height))
+        path.addLine(to: CGPoint(x: 0.44221*width, y: 0.57057*height))
+        path.addCurve(to: CGPoint(x: 0.55502*width, y: 0.57057*height), control1: CGPoint(x: 0.47409*width, y: 0.54242*height), control2: CGPoint(x: 0.52313*width, y: 0.54242*height))
+        path.addLine(to: CGPoint(x: 0.97307*width, y: 0.93959*height))
+        path.addCurve(to: CGPoint(x: width, y: 0.99848*height), control1: CGPoint(x: 0.99023*width, y: 0.95474*height), control2: CGPoint(x: width, y: 0.9761*height))
+        path.addLine(to: CGPoint(x: width, y: -0.33867*height))
+        path.closeSubpath()
+        return path
+    }
 }
