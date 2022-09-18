0e1560372f73d24a159c81dde06a8d5844adb62c
Ibrahim <i-m-6@hotmail.com>
2022-09-18 18:08:16 +0300

    feat: add page to be specific for TabBar

---
+++ FirstUI/TabBarUI.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarUI.swift
+//  FirstUI
+//
+//  Created by ibrahim asiri on 22/02/1444 AH.
+//
+
+import SwiftUI
+
+struct TabBarUI: View {
+    var body: some View {
+        Text("The First")
+        
+    }
+}
+
+struct TabBarUI_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarUI()
+    }
+}
